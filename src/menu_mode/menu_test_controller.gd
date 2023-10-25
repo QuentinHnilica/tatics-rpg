66c51f7f087b95009d729958fd5fc09ff568ce72
TheRedSong <coledutil@vivaldi.net>
2023-10-25 01:50:01 -0400

    update
    update

--- src/menu_mode/menu_test_controller.gd
+++ src/menu_mode/menu_test_controller.gd
@@ -1,29 +1,34 @@
 extends Control
 
+func _ready():
+	for option in $Margin/Options.get_children():
+		option.connect("pressed",Callable(self,"_on_" + option.name + "_pressed"))
 
-func is_mouse_hover_button():
-	if $Margin/Options.visible:
-		for option in $Margin/Options.get_children():
-			if option.get_global_rect().has_point(get_viewport().get_mouse_position()): 
-				return true
-	return false
-
-
-func _on_overworld_test_pressed():
+func _on_OverworldTest_pressed():
+	print("test overworld")
 	get_tree().change_scene_to_file("res://scenes/overworld_mode.tscn")
 
 
-func _on_battle_test_pressed():
+func _on_BattleTest_pressed():
+	print("test battle")
 	get_tree().change_scene_to_file("res://scenes/battle_mode.tscn")
 
 
-func _on_vn_test_pressed():
+func _on_VNTest_pressed():
+	print("test vn")
 	get_tree().change_scene_to_file("res://scenes/vn_mode.tscn")
 
 
-func _on_menu_test_pressed():
+func _on_MenuTest_pressed():
+	print("test menu")
 	get_tree().change_scene_to_file("res://scenes/menu_mode.tscn")
 
 
-func _on_test_level_pressed():
+func _on_TestLevel_pressed():
+	print("test level")
 	get_tree().change_scene_to_file("res://assets/prefabs/test_level.tscn")
+
+
+func _on_DialogueTest_pressed():
+	print("test dialogue")
+	DialogueManager.show_example_dialogue_balloon(load("res://assets/dialogues/test_dialogue.dialogue"), "start")
